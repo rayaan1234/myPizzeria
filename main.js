b14cb5a2099e407f7497c511041b492606ab8c2c
rayaan1234 <83221851+rayaan1234@users.noreply.github.com>
2021-09-17 19:55:54 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-name_of_the_student_array=[];
-function submit()
-{
-    var name_1 = document.getElementById("name_of_the_student_1").value;
-    var name_2 = document.getElementById("name_of_the_student_2").value;
-    var name_3 = document.getElementById("name_of_the_student_3").value;
-    var name_4 = document.getElementById("name_of_the_student_4").value;
-    name_of_the_student_array.push(name_1);
-    name_of_the_student_array.push(name_2);
-    name_of_the_student_array.push(name_3);
-    name_of_the_student_array.push(name_4);
-    console.log(name_of_the_student_array);
-    document.getElementById("display_name").innerHTML = name_of_the_student_array;
-    document.getElementById("submit_button").style.display = "none";
-    document.getElementById("sort_button").style.display = "inline-block";
+menu_list_array = ["Veg Margherita Pizza",
+"Chicken Tandoori Pizza",
+"Veg Supreme Pizza",
+"Paneer Tikka Pizza",
+"Deluxe Veggie Pizza",
+"Veg Extravaganza Pizza"];
 
+function getmenu(){
+var htmldata="";
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+htmldata=htmldata+ menu_list_array[i] + '<br>'
+}
+document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata=""
+for(var i=0;i<menu_list_array.length;i++){
+htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+}
+document.getElementById("display_addedmenu").innerHTML = htmldata;
 
 }
-function sorting()
-{
-    name_of_the_student_array.sort();
-    console.log(name_of_the_student_array);
-    document.getElementById("display_name").innerHTML = name_of_the_student_array;
+
+function add_top(){
+var item=document.getElementById("add_item").value;
+menu_list_array.push(item);
+add_item();
 }
